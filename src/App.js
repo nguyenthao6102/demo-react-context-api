5bac0c2d04b37c70ab341843a6912fb6dbc03c64
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-04-17 22:04:02 +0700

    demo react context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NumberContext from "./contexts/NumberContext";
+import NumberProvider from "./components/NumberProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<NumberProvider>
+			<div className="App">
+				<NumberContext.Consumer>
+					{({ number, updateNumber }) => (
+						<div>
+							<h2>{number}</h2>
+							<button onClick={updateNumber}>Update Number</button>
+						</div>
+					)}
+				</NumberContext.Consumer>
+			</div>
+		</NumberProvider>
+	);
 }
 
 export default App;
